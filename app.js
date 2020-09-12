15c790c08b060fbd803d6b9f41215348d6fc487f
Folajimi Adekoya <fadekoya@zendesk.com>
2020-09-12 23:47:05 +0200

    fix the tiles

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 /*eslint-env browser*/
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-global.document = new JSDOM(
-  "/Users/fadekoya/udacitystudy/Javascript/index.html"
-).window.document;
-
 // Create Dino Constructor
 
 function Dino(species, weight, height, diet, where, when, fact) {
@@ -19,124 +13,198 @@ function Dino(species, weight, height, diet, where, when, fact) {
 }
 
 // Create Dino Objects
-const dinos = require("/Users/fadekoya/udacitystudy/Javascript/dino.json");
+//const dinos = require("/Users/fadekoya/udaProjectRepo/intermediate_js_udacity/dino.json");
 
-//const pear = JSON.parse(dinos);
-//var obj = JSON.parse(json).reduce((a, v) => { a[v.key] = v.value; return a; }, {});
+fetch("dino.json")
+  .then(response => response.json())
+  .then(dinoData => {
+    let uniqueDino = [];
+    uniqueDino = dinoData.Dinos.map(
+      dino =>
+        new Dino(
+          dino.species,
+          dino.weight,
+          dino.height,
+          dino.diet,
+          dino.where,
+          dino.when,
+          dino.fact
+        )
+    );
+    //return uniqueDino;
 
-dinos.Dinos.map(function (dino) {
-  const aUniqueDino = new Dino(
-    dino.species,
-    dino.weight,
-    dino.height,
-    dino.diet,
-    dino.where,
-    dino.when,
-    dino.fact
-  );
-  //console.log(aUniqueDino);
-});
+    // Generate Tiles for each Dino in Array
+    let dinoTileInfos = uniqueDino;
+    let humanTileInfo = getHumanData();
 
-//console.log(dinos.Dinos);
+    dinoTileInfos.splice(4, 0, humanTileInfo);
 
-// Create Human Object
+    //  Add tiles to DOM
+    function addTilesToDOM(humanName) {
+      let grid = document.getElementById("grid");
+      dinoTileInfos.map(dino => {
+        let tile = document.createElement("div");
+        let title = document.createElement("h3");
+        let fact = document.createElement("p");
+        let image = document.createElement("img");
 
-const human = (function () {
-  let name = document.getElementById("name");
-  let heightFeet = document.getElementById("feet");
-  let heightInches = document.getElementById("inches");
-  let weight = document.getElementById("weight");
-  let diet = document.getElementById("diet");
+        // tile to grid
+        tile.className = "grid-item";
 
-  function getName() {
-    return name;
-  }
+        // create tile header
+        title.className = "h3";
+        if (dino.species) {
+          title.innerHTML = dino.species;
+        } else {
+          title.innerHTML = humanName;
+        }
+        // add facts to tile
+        fact.className = "p";
+        let facts = dino.fact;
+        let randomFact = "";
 
-  function getHeightFeet() {
-    return heightFeet;
-  }
+        if (facts) {
+          randomFact = facts[Math.floor(Math.random() * facts.length)];
+        }
+        if (dino.species == "Pigeon") {
+          fact.innerHTML = dino.fact[0];
+        } else {
+          fact.innerHTML = randomFact;
+        }
+        // add image to tile
+        image.className = "img";
+        if (dino.species) {
+          image.src = `./images/${dino.species.toLowerCase()}.png`;
+        } else {
+          image.src = `./images/${human.image}`;
+        }
 
-  function getHeightInches() {
-    return heightInches;
-  }
+        tile.appendChild(title);
+        tile.appendChild(fact);
+        tile.appendChild(image);
+        grid.appendChild(tile);
+      });
+    }
 
-  function getHeight() {
-    return getHeightFeet() + "Feet " + getHeightInches() + " Inches";
-  }
+    const aDinoObject = uniqueDino;
+    aDinoObject.map(dino => {
+      // Create Dino Compare Method 1
+      // NOTE: Weight in JSON file is in lbs, height in inches.
 
-  function getWeight() {
-    return weight;
-  }
+      const compareWeight = function () {
+        if (human.weight < aDinoObject.weight) {
+          return `${aDinoObject.species} weighs ${
+            aDinoObject.weight - human.weight
+          } more than ${human.name}!`;
+        } else if (human.weight > aDinoObject.weight) {
+          return `${human.name} weighs ${
+            human.weight - aDinoObject.weight
+          } more than ${dino.species}`;
+        } else {
+          return `${human.name} and ${aDinoObject.species} weigh the same! wow!. i almost could not belive it`;
+        }
+      };
 
-  function getDiet() {
-    return diet;
-  }
+      // Create Dino Compare Method 2
+      // NOTE: Weight in JSON file is in lbs, height in inches.
 
-  return {
-    Name: getName,
-    Height: getHeight,
-    Weight: getWeight,
-    Diet: getDiet
-  };
-})();
+      const compareHeight = function () {
+        if (human.height < aDinoObject.height) {
+          return `${aDinoObject.species} is taller than ${human.name} by ${
+            aDinoObject.height - human.height
+          } ft !`;
+        } else if (human.height > aDinoObject.height) {
+          return `${human.name} is taller than ${dino.species} by ${
+            human.weight - aDinoObject.weight
+          }! `;
+        } else {
+          return `${human.name} and ${aDinoObject.species} are as tall as each other, who would have thot!`;
+        }
+      };
 
-console.log(human);
+      // Create Dino Compare Method 3
+      // NOTE: Weight in JSON file is in lbs, height in inches.
+
+      const compareDiet = function () {
+        if (human.diet === "Carnivor" && aDinoObject.diet === "Carnivor") {
+          return `${aDinoObject.species} and ${human.name} but love meat`;
+        } else if (
+          human.diet === "herbavor" &&
+          aDinoObject.diet === "herbavor"
+        ) {
+          return `${human.name} and ${dino.species} are Vegetarians`;
+        } else {
+          return `${human.diet} and ${aDinoObject.diet} have different diets , no suprise here`;
+        }
+      };
+    });
+
+    //Hide form from screen
+    function toggleEntryForm() {
+      const HideForm = document.getElementById("dino-compare");
+      HideForm.classList.add("hide");
+    }
+
+    //On button click, prepare and display infographic
+    const compareDinos = document.getElementById("btn");
+    compareDinos.addEventListener("click", function () {
+      const human = getHumanData();
+
+      // Remove form from screen
+      toggleEntryForm();
+      // Add tiles to DOM
+      addTilesToDOM(human.name);
+    });
+  });
 
 // Use IIFE to get human data from form
+// Create Human Object
 
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
+function getHumanData() {
+  const human = (function () {
+    const name = document.getElementById("name").value;
+    const heightFeet = document.getElementById("feet").value;
+    const heightInches = document.getElementById("inches").value;
+    const weight = document.getElementById("weight").value;
+    const diet = document.getElementById("diet").value;
+    const image = "human.png";
 
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
+    function getName() {
+      return name;
+    }
 
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
+    function getHeightFeet() {
+      return heightFeet;
+    }
 
-// Generate Tiles for each Dino in Array
+    function getHeightInches() {
+      return heightInches * 0.083;
+    }
 
-// Add tiles to DOM
+    function getHeight() {
+      return getHeightFeet() + getHeightInches();
+    }
 
-// Remove form from screen
+    function getWeight() {
+      return weight;
+    }
 
-// On button click, prepare and display infographic
-// Create Dino Compare Method 3
+    function getDiet() {
+      return diet;
+    }
 
-const dinObject = aUniqueDino[i];
-const compareWeight = function () {
-  if (human.weight < dinObject.weight) {
-    return `${dinObject.species} weighs ${
-      dinObject.weight - human.weight
-    } more than ${human.name}!`;
-  } else if (human.weight > dinObject.weight) {
-    return `${human.name} weighs ${human.weight - dinObject.weight} more than ${
-      dino.species
-    }`;
-  } else {
-    return `${human.name} and ${dinObject.species} weigh the same! wow!. i almost could not belive it`;
-  }
-};
+    function getImage() {
+      return image;
+    }
 
-const compareHeight = function () {
-  if (human.height < dinObject.height) {
-    return `${dinObject.species} is taller than ${human.name} by ${
-      dinObject.height - human.height
-    } ft !`;
-  } else if (human.height > dinObject.height) {
-    return `${human.name} is taller than ${dino.species} by ${
-      human.weight - dinObject.weight
-    }! `;
-  } else {
-    return `${human.name} and ${dinObject.species} are as tall as each other, who would have thot!`;
-  }
-};
+    return {
+      name: getName(),
+      height: getHeight(),
+      weight: getWeight(),
+      diet: getDiet(),
+      image: getImage()
+    };
+  })();
 
-const compareDiet = function () {
-  if (human.diet === "Carnivor" && dinObject.diet === "Carnivor") {
-    return `${dinObject.species} and ${human.name} but love meat`;
-  } else if (human.diet === "herbavor" && dinObject.diet === "herbavor") {
-    return `${human.name} and ${dino.species} are Vegetarians`;
-  } else {
-    return `${human.diet} and ${dinObject.diet} have different diets , no suprise here`;
-  }
-};
+  return human;
+}
